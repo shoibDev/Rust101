c16616220592b359844b3a4238138adc8bdb67e8
ShoibDev <sediq1973@gmail.com>
2025-05-29 13:56:25 -0400

    shopping cart

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// A shopping cart project 
 fn main() {
-    println!("Hello, world!");
+    let mut shopping_cart: [&str; 10] = ["Empty"; 10];
+    show_items(shopping_cart);
+}
+
+fn show_items(shopping_cart: [&str; 10]) {
+    for item in shopping_cart.iter() {
+        println!("{}", item);
+    }
 }
