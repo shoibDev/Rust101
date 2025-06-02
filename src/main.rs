c4f31fea44548a6945345b1a1c9c0300fd23bb24
ShoibDev <sediq1973@gmail.com>
2025-06-02 16:38:55 -0400

    Tokenizer complete

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
+use std::io;
+// Break Expression into tokens
 #[derive(Debug)]
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+enum Token {
+    Number(u8),
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    LParentheses,
+    RParentheses,
 }
-
 fn main() {
-    let mut user1: User = User {
-        username: String::from("ShubyDrive"),
-        email: String::from("shub@gmail.com"),
-        sign_in_count: 1,
-        active: true,
-    };
 
-    let name: String = user1.username;
+    let mut token_list: Vec<Token> = Vec::new();
+    let mut expression: String = String::new();
 
-    println!("{}", name);
-    user1.username = String::from("not_ShubyDrive");
+    println!("Enter expression to be evaluated: ");
 
-    let user2: User = build_user(String::from("test@example.com"), String::from("tester"));
-    println!("rect: {:#?}", user2);
-}
+    io::stdin()
+        .read_line(&mut expression)
+        .expect("Failed to read line");
+    expression = expression.trim().to_string();
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
+    // Tokenize the expression
+    for char in expression.chars() {
+        if char == ' ' { continue }
+        match char {
+            '+' => token_list.push(Token::Addition),
+            '-' => token_list.push(Token::Subtraction),
+            '*' => token_list.push(Token::Multiplication),
+            '/' => token_list.push(Token::Division),
+            '(' => token_list.push(Token::LParentheses),
+            ')' => token_list.push(Token::RParentheses),
+            '0'..='9' => {
+                let digit = char.to_digit(10).unwrap() as u8;
+                token_list.push(Token::Number(digit));
+            }
+            _ => {
+                println!("Unexpected character: {}", char);
+            }
+        }
     }
 }
