e99e63382628e08c715ee789834e58046c811fc4
ShoibDev <sediq1973@gmail.com>
2025-06-12 00:01:06 -0400

    collections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut v: Vec<i32> = vec![1, 2, 3, 4, 5];
+    
+    let third: &i32 = &v[2]; // Immutable borrow here
+    v.push(6);         // Mutable borrow here 
+
+    println!("the third element is: {}", third); // refenching the immuatbla refeence
+
+
 }
