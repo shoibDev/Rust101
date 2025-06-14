68bfcdeb0f987e1ff84c8ac6324c653f67179d63
ShoibDev <sediq1973@gmail.com>
2025-06-14 19:50:06 -0400

    learnt the collections so far :D

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
 fn main() {
-
-    let mut v: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let blue: String = String::from("Blue");
+    let yellow: String = String::from("Yellow");
     
-    let third: &i32 = &v[2]; // Immutable borrow here
-    v.push(6);         // Mutable borrow here 
-
-    println!("the third element is: {}", third); // refenching the immuatbla refeence
-
-
+    let mut score: HashMap<String, i32> = HashMap::new();
+    
+    score.insert(blue, 10);
+    score.insert(yellow, 10); // this will move the ownership of the variables into the hashmap
+    
+    let (blue, score) = score.remove_entry("Blue").unwrap_or_else(|| (String::from("Null"), 0));
+    
 }
