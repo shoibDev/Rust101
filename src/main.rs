32382767c57fd2790f7740d7d5b93ec340703e5a
ShoibDev <sediq1973@gmail.com>
2025-07-18 21:17:15 -0400

    Main updated

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use std::collections::HashMap;
 fn main() {
-    let blue: String = String::from("Blue");
-    let yellow: String = String::from("Yellow");
-    
-    let mut score: HashMap<String, i32> = HashMap::new();
-    
-    score.insert(blue, 10);
-    score.insert(yellow, 10); // this will move the ownership of the variables into the hashmap
-    
-    let (blue, score) = score.remove_entry("Blue").unwrap_or_else(|| (String::from("Null"), 0));
-    
+    println!("This is main!");
 }
